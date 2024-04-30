f9dc3e75f1ef8351d06e637102962063cc31cf9b
maggarwal25 <maggarwal@echo360.com>
2024-04-29 23:38:22 -0400

    updates to mmd loss

--- generate_mass_spring_data.py
+++ generate_mass_spring_data.py
@@ -1,8 +1,12 @@
 import torch
 
 def generate_mass_spring_data(k, m, x0, v0, dt, num_steps):
-    w = torch.sqrt(k / m)
+
+    w = torch.sqrt(torch.tensor(k/m))
     t = torch.arange(num_steps).float() * dt
     x = x0 * torch.cos(w * t) + v0 / w * torch.sin(w * t)
     v = -x0 * w * torch.sin(w * t) + v0 * torch.cos(w * t)
+
     return x, v
+
+x, v = generate_mass_spring_data(1, 1, 1, 1, 1, 10)
