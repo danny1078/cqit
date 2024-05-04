0e937c6fdf8a9c5c865e487d43443e3fec5ae948
Danny Han <handanny2014@gmail.com>
2024-05-04 13:44:27 -0400

    3d conserved quantities func

---
+++ 3d_sho_conserved_qts.py
@@ -0,0 +1,5 @@
+import torch
+def energy_angular_momentum(x_0, v_0, m, k):
+    energy = 0.5 * m * torch.sum(v_0 ** 2, dim=1) + 0.5 * k * torch.sum(x_0 ** 2, dim=1)
+    angular_momentum = torch.linalg.cross(x_0, m * v_0)
+    return energy, angular_momentum
